0a4aa8f7b1f5e12a724f37fb867897d052d7ea8d
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2022-09-24 00:19:11 +0200

    Init

--- front-end/hooks/useSyncUser.ts
+++ front-end/hooks/useSyncUser.ts
@@ -1,6 +1,6 @@
 import { gql, useLazyQuery } from '@apollo/client';
-import { CognitoUser } from '../graphql/API';
-import { getCognitoUser } from '../graphql/queries';
+import { CognitoUser } from '../src/API';
+import { getCognitoUser } from '../src/graphql/queries';
 
 export const useGetUser = () => {
   const [call, { loading, error, data }] = useLazyQuery<{ getCognitoUser: CognitoUser }>(gql(getCognitoUser), {
